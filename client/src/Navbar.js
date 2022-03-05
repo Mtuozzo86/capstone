567f04cd13837eb7f6529a6c446fc3105753f5a3
Mtuozzo86 <mtuozzo86@gmail.com>
2022-03-05 03:29:23 -0500

    styling

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,13 +1,20 @@
 import "./Navbar.css";
 import { BsSearch } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navbar-header">
-      <p>I Know A Guy</p>
+      <Link to="/">
+        <p className="title">I Know A Guy</p>
+      </Link>
+
       <div className="navbar">
         <p>Log In</p>
-        <p>Create Acct</p>
+        <Link to="/create">
+          <p>Create Acct</p>
+        </Link>
+
         <p>
           <BsSearch />
         </p>
