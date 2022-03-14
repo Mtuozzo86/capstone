eae8a0a96c244639755cb14ae1949dd6ca4d4a6f
Mtuozzo86 <mtuozzo86@gmail.com>
2022-03-14 17:39:45 -0400

    front end

--- client/src/OwnerLandingPage.js
+++ client/src/OwnerLandingPage.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { FaAddressCard, FaClock } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { BsFillPersonFill } from "react-icons/bs";
 import CreateOwnerAddress from "./OwnerForms/CreateOwnerAddress";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./OwnerLandingPage.css";
 
 function OwnerLandingPage({ loggedInUser }) {
-  console.log(loggedInUser);
+  console.log(loggedInUser.owner_addresses[0].street);
   function handleLogout() {
     fetch("/logout", {
       method: "DELETE",
@@ -15,29 +17,47 @@ function OwnerLandingPage({ loggedInUser }) {
 
   const [edit, setIsEditting] = useState(false);
   const [openCalendar, setOpenCalendar] = useState(false);
+  const [landingPageContainer, setLandingPageContainer] = useState(true);
 
   function handleAddressClick() {
-    setIsEditting((edit) => !edit);
+    setIsEditting(true);
     setOpenCalendar(false);
+    setLandingPageContainer(false);
   }
 
   function handleCalenderClick() {
     setIsEditting(false);
-    setOpenCalendar((openCalender) => !openCalender);
+    setOpenCalendar(true);
+    setLandingPageContainer(false);
+  }
+
+  function handleOwnerClick() {
+    setLandingPageContainer(true);
+    setOpenCalendar(false);
+    setIsEditting(false);
   }
 
   return (
     <>
       <div className="landing-page-container-home">
         <div className="react-icons">
+          <li onClick={handleOwnerClick}>
+            <BsFillPersonFill />
+          </li>
           <li onClick={handleAddressClick}>
-            <FaAddressCard />
+            <BsFillPersonLinesFill />
           </li>
           <li onClick={handleCalenderClick}>
             <FaClock />
           </li>
         </div>
-        {loggedInUser.first_name}
+
+        {landingPageContainer ? (
+          <div className="landing-page-info-container">
+            {loggedInUser.first_name}
+            {loggedInUser.owner_addresses[0].street}
+          </div>
+        ) : null}
 
         {edit ? <CreateOwnerAddress loggedInUser={loggedInUser} /> : null}
         <div className="calendar">{openCalendar ? <Calendar /> : null}</div>
