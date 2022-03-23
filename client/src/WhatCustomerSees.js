2e49d99935afdd5c493f1464082c01a4d5220c38
Mtuozzo86 <mtuozzo86@gmail.com>
2022-03-23 11:14:30 -0400

    customer side

--- client/src/WhatCustomerSees.js
+++ client/src/WhatCustomerSees.js
@@ -4,17 +4,17 @@ import { useParams } from "react-router-dom";
 function WhatCustomerSees() {
   let webAddress = useParams();
   const [business, setBusiness] = useState([]);
-  console.log(business);
+
   useEffect(() => {
     fetch(`/owners/${webAddress.business}`)
       .then((resp) => resp.json())
       .then((stuff) => setBusiness(stuff[0]));
-  }, [webAddress.business]);
+  });
 
   return (
     <div>
       <div>
-        <h2>Owner: {business.first_name + business.last_name}</h2>
+        <h2>Owner: {business.first_name + " " + business.last_name}</h2>
       </div>
       <div>Name of business: {business.business}</div>
     </div>
