3a721cc7d4fdb10dde4cb693d8dc479c3c3b0164
Mtuozzo86 <mtuozzo86@gmail.com>
2022-03-30 12:03:59 -0400

    updates for customers css

--- client/src/CustomerComponents/BusinessFound.js
+++ client/src/CustomerComponents/BusinessFound.js
@@ -6,11 +6,12 @@ function BusinessFound({ companyInfo }) {
   console.log(companyInfo);
 
   return (
-    <div className="business-found-wrapper">
+    <div className="business-found">
       <BusinessNavbar />
-      <div>
+      <div className="business-found-company-name">
         <h1>{companyInfo.business}</h1>
       </div>
+      <div></div>
     </div>
   );
 }
