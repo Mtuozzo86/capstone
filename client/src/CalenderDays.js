34cf84d2efb40fed6f795def209a573c8d7e9208
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-04-02 22:27:31 -0400

    trouble with calendar

--- client/src/CalenderDays.js
+++ client/src/CalenderDays.js
@@ -1,11 +1,45 @@
+import { useState } from "react";
+
 function CalendarDays({ day }) {
+  const nineToFive = [
+    "9:00am - 9:45am",
+    "10:00am - 10:45am",
+    "11:00am - 11:45am",
+    "12:00pm - 12:45pm",
+    "1:00pm - 1:45pm",
+    "2:00pm - 2:45pm",
+    "3:00pm - 3:45pm",
+    "4:00pm - 4:45pm",
+  ];
+  const [availability, setAvailability] = useState(nineToFive);
+  const [bookings, setBookings] = useState([]);
+
+  const timeFrame = availability.map((time) => {
+    return (
+      <li onClick={() => handleClick(time)} key={time}>
+        {time}
+      </li>
+    );
+  });
+
+  function handleClick(times) {
+    const removeTime = availability.filter((time) => time !== times);
+    const clickedTimeFrame = times;
+    const listOfBookings = [...bookings, clickedTimeFrame];
+    setBookings(listOfBookings);
+    setAvailability(removeTime);
+  }
   return (
-    <h1>
-      {day.day}
-      <>{day.appointments}</>
-      <br />
-      {day.booked}
-    </h1>
+    <div>
+      <h1>{day}</h1>
+      <div>{timeFrame}</div>
+      <h2>Booked Days</h2>
+      <div>
+        {bookings.map((booked) => {
+          return <li key={booked}>{booked}</li>;
+        })}
+      </div>
+    </div>
   );
 }
 
--- client/src/CalenderTest.js
+++ client/src/CalenderTest.js
@@ -5,49 +5,16 @@ import CalendarDays from "./CalenderDays";
 // import CalenderDays from "./CalenderDays";
 
 function CalenderTest() {
-  const nineToFive = [
-    "9:00am - 9:45am",
-    "10:00am - 10:45am",
-    "11:00am - 11:45am",
-    "12:00pm - 12:45pm",
-    "1:00pm - 1:45pm",
-    "2:00pm - 2:45pm",
-    "3:00pm - 3:45pm",
-    "4:00pm - 4:45pm",
-  ];
-  const [availability, setAvailability] = useState(nineToFive);
-  const [bookings, setBookings] = useState([]);
-  const [value, onChange] = useState(new Date());
+  const [value, setValue] = useState(new Date());
 
-  const selectedDay = {
-    day: value.toLocaleDateString(),
-    availability: availability,
-    booked: bookings,
-  };
-
-  console.log(selectedDay);
-
-  const timeFrame = availability.map((time) => {
-    return (
-      <li onClick={() => handleClick(time)} key={time}>
-        {time}
-      </li>
-    );
-  });
-
-  function handleClick(times) {
-    const removeTime = availability.filter((time) => time !== times);
-    const clickedTimeFrame = times;
-    const listOfBookings = [...bookings, clickedTimeFrame];
-    setBookings(listOfBookings);
-    setAvailability(removeTime);
+  function handleChange(e) {
+    setValue(e);
   }
 
   return (
     <div>
-      {timeFrame}
-      <Calendar onChange={onChange} value={value} calendarType={"US"} />
-      <CalendarDays day={selectedDay} />
+      <Calendar onChange={handleChange} value={value} calendarType={"US"} />
+      <CalendarDays day={value.toLocaleDateString()} />
     </div>
   );
 }
