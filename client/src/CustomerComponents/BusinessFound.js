28d918afccb59efc7e37818c85e90099e782d819
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-04-13 17:48:37 -0400

    nothing works

--- client/src/CustomerComponents/BusinessFound.js
+++ client/src/CustomerComponents/BusinessFound.js
@@ -9,6 +9,9 @@ function BusinessFound({ companyInfo, urlParam }) {
   return (
     <div className="business-found">
       <BusinessNavbar bio={companyInfo.biography} urlParam={urlParam} />
+      <Routes>
+        <Route path={`/about`} element={<About />} />
+      </Routes>
     </div>
   );
 }
