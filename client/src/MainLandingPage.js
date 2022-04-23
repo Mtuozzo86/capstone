ee97eb268022cec8c3739ab01dd9913c460bbb4e
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-04-22 20:41:04 -0400

    testing search, not finished

--- client/src/MainLandingPage.js
+++ client/src/MainLandingPage.js
@@ -1,19 +1,38 @@
-import Navbar from "./Navbar";
+import { useState, useEffect } from "react";
 import "./CSS/MainLandingPage.css";
 import MainLandingPageNavbar from "./MainLandingPageNavbar";
 
 function MainLandingPage() {
+  const [searched, setSearched] = useState("");
+  const [results, setResults] = useState([]);
+  console.log(results);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/owners")
+      .then((resp) => resp.json())
+      .then((data) => setResults(data));
+  }
+
+  const searchResults = results.filter((result) => {
+    return;
+  });
+
   return (
     <div className="main-landing-page-container">
       <MainLandingPageNavbar />
 
       <div className="main-landing-page-items">
         <div className="item">
-          <input className="main-landing-page-search" type="text" />
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={(e) => setSearched(e.target.value)}
+              className="main-landing-page-search"
+              type="text"
+            />
+          </form>
         </div>
-        <div className="item">
-          <h4>Browse</h4>
-        </div>
+        <div className="item"></div>
       </div>
     </div>
   );
