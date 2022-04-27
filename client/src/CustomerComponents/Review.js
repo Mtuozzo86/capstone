31f5a4c93899a364c3f63e5aaf80cff111dabc5f
Mtuozzo86 <mtuozzo86@gmail.com>
2022-04-27 00:01:10 -0400

    review star renders

--- client/src/CustomerComponents/Review.js
+++ client/src/CustomerComponents/Review.js
@@ -1,16 +1,19 @@
+import { AiFillStar } from "react-icons/ai";
+
 function Review({ body, rating, name, date }) {
   const dateFormat = new Date(date);
+
   function convertRating(rating) {
     let emoji = "";
     for (let i = 0; i < rating; i++) {
-      emoji += "🌟";
+      emoji += "⭐";
     }
     return emoji;
   }
 
   return (
     <div className="review-box">
-      <div className="review-box-rating">{convertRating(rating)}</div>
+      <span className="review-box-rating">{convertRating(rating)}</span>
       <div>
         <p>{body}</p>
         <p className="review-box-commenter">
