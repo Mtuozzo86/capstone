6af73fba41d3a54aea1c60738adb2d61868c05d2
Mtuozzo86 <mtuozzo86@gmail.com>
2022-04-28 12:14:41 -0400

    search css

--- client/src/MainLandingPage/Result.js
+++ client/src/MainLandingPage/Result.js
@@ -4,8 +4,8 @@ function Result({ business, website, image }) {
   return (
     <Link to={`/business/${website}`}>
       <h3>
-        {business}
         <img className="result-image-icon" src={image} alt="" />
+        {business}
       </h3>
     </Link>
   );
