e9bee1dfd039b70a63ceb6c0b7b2cb37f74ddb9c
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-04-28 00:33:54 -0400

    search

--- client/src/MainLandingPage/Result.js
+++ client/src/MainLandingPage/Result.js
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
 function Result({ business, website }) {
-  console.log(business);
-
   return (
     <Link to={`/business/${website}`}>
       <h3>{business}</h3>
--- client/src/MainLandingPage/SearchResults.js
+++ client/src/MainLandingPage/SearchResults.js
@@ -1,9 +1,6 @@
 import Result from "./Result";
 
 function SearchResults({ results }) {
-  if (results.length === 0) {
-    return <p>Nothing found</p>;
-  }
   const listOfResults = results.map((result) => {
     return (
       <Result
