52d93a4efe15d03ac30240be270a8eac62f0fcc1
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-07-02 16:57:29 -0400

    trying custom review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,13 @@ class Review < ApplicationRecord
   belongs_to :owner
   belongs_to :client
 
-  validates :body, presence: true
-  validates_associated :client
+  validates :body, presence: true, message: "Can't be empty"
+  # validates_associated :client
+  # validate :client_review
+
+  # def client_review
+  #   if self.client === nil
+  #     errors.add(:base, "Please leave your name") if self.client.blank?
+  #   end
+  # end
 end
